763420de4796691eb2869c705d2099981d861f38
yuzuka-side-0107 <153477819+yuzuka-side-0107@users.noreply.github.com>
2023-12-27 12:48:12 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_005</title>
+</head>
+
+<body>
+  <p>
+  <?php
+  $item_date = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+  print_r($item_date);
+  ?>
+  </p>
+</body>
+
+</html>
